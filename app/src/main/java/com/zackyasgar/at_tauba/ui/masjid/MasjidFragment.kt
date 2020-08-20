dfd29b32f2e2650a1986a125ed72fc512ba31df0
arifahmadalfian <arifahmadalfian@gmail.com>
2020-08-20 21:42:58 +0700

    actionbutton pada FloatingActionButton membawa data kordinat dan berpindah ke aplikasi google maps

--- app/src/main/java/com/zackyasgar/at_tauba/ui/masjid/MasjidFragment.kt
+++ app/src/main/java/com/zackyasgar/at_tauba/ui/masjid/MasjidFragment.kt
@@ -1,9 +1,11 @@
 package com.zackyasgar.at_tauba.ui.masjid
 
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.DialogInterface
 import android.content.Intent
-import android.content.Intent.parseIntent
+import android.content.Intent.*
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -16,7 +18,7 @@ import kotlinx.android.synthetic.main.fragment_masjid.*
 import android.content.Intent.getIntent as intentGetIntent
 
 
-class MasjidFragment : Fragment() {
+class MasjidFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +27,33 @@ class MasjidFragment : Fragment() {
         // Inflate the layout for this fragment
         // Objects.requireNonNull(supportActionBar)?.setDisplayHomeAsUpEnabled(true)
         return inflater.inflate(R.layout.fragment_masjid, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        btn_fab_maps.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_fab_maps -> getGoogleMaps()
+        }
+    }
+
+    private fun getGoogleMaps() {
+        val gDestination = "-6.937810,107.655653"
+        try {
+            val uri = Uri.parse("https://www.google.co.in/maps/dir/?api=1&destination=$gDestination")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setPackage("com.google.android.apps.maps")
+            intent.flags = FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException){
+            val uri = Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps")
+            val intent = Intent(ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 
 }
