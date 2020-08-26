4780b03c693c64f570eca6a575eab09683c9c589
arifahmadalfian <arifahmadalfian@gmail.com>
2020-08-27 00:25:31 +0700

    Berhasil menambahkan data Pengajian.kt ke firebase

--- app/src/main/java/com/zackyasgar/at_tauba/admin/DataPengajianFragment.kt
+++ app/src/main/java/com/zackyasgar/at_tauba/admin/DataPengajianFragment.kt
@@ -1,5 +1,6 @@
 package com.zackyasgar.at_tauba.admin
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.app.TimePickerDialog.OnTimeSetListener
@@ -9,7 +10,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.text.format.DateFormat
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.FirebaseDatabase
 import com.zackyasgar.at_tauba.R
 import kotlinx.android.synthetic.main.fragment_data_pengajian.*
 import java.util.*
@@ -31,44 +34,108 @@ class DataPengajianFragment : Fragment(), View.OnClickListener{
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        pg_pengajian.visibility = View.GONE
+
         btn_pengajian_tgl.setOnClickListener(this)
         btn_pengajian_jam.setOnClickListener(this)
+
+        btn_pengajian_tambah.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_pengajian_tgl -> {
-                val calendar = Calendar.getInstance()
-                val c_year = calendar.get(Calendar.YEAR)
-                val c_month = calendar.get(Calendar.MONTH)
-                val c_day = calendar.get(Calendar.DAY_OF_MONTH)
-
-                datePickerDialog = DatePickerDialog(
-                    context as Context, DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-                        tv_pengajian_tgl.text = ("$dayOfMonth-${month + 1}-$year")  //ditambah satu karena index MOUNT di mulai dari nol (0-11)
-                    }, c_year, c_month, c_day
-                )
-
-                datePickerDialog?.show()
+                getTanggalPengajian()
             }
 
             R.id.btn_pengajian_jam -> {
-                val calendar = Calendar.getInstance()
+                getJamPengajian()
+            }
+            R.id.btn_pengajian_tambah -> {
+                // untuk UUID/ID random di firebase
+                val idPengajian = UUID.randomUUID().toString()
 
-                timePickerDialog = TimePickerDialog(
-                    context as Context, OnTimeSetListener { _, hourOfDay, minute ->
-                        tv_pengajian_jam.text = ("$hourOfDay:$minute")
-                    },
-                    // calender pertamakali di buka
-                    calendar[Calendar.HOUR_OF_DAY], calendar[Calendar.MINUTE],
-                    // cek format 24 jam
-                    DateFormat.is24HourFormat(context as Context)
-                )
+                // mengambil text dari fragment_data_pengajian yang sudah di isi
+                val pTema = et_pengajian_tema.text.toString().trim()
+                val pJudul = et_pengajian_judul.text.toString().trim()
+                val pTanggal = tv_pengajian_tgl.text.toString().trim()
+                val pJam = tv_pengajian_jam.text.toString().trim()
+                val pIsi = et_pengajian_isi.text.toString().trim()
 
-                timePickerDialog?.show()
+                when {
+                    pTema.isEmpty() -> et_pengajian_tema.error = "Tema tidak boleh kosong"
+                    pJudul.isEmpty() -> et_pengajian_judul.error = "Judul tidak boleh kosong"
+                    pTanggal == "Tanggal Pengajian" -> Toast.makeText(context, "Masukan Tanggal", Toast.LENGTH_SHORT).show()
+                    pJam == "Waktu Pengajian" -> Toast.makeText(context, "Masukan Jam", Toast.LENGTH_SHORT).show()
+                    pIsi.isEmpty() -> et_pengajian_isi.error = "Isi tidak boleh kosong"
+                    else -> getTambahDataPengajian(idPengajian, pTema, pJudul, pTanggal, pJam, pIsi)
+                }
             }
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getTambahDataPengajian(
+        idPengajian: String,
+        pTema: String,
+        pJudul: String,
+        pTanggal: String,
+        pJam: String,
+        pIsi: String
+    ) {
+        // menampilkan progress bar
+        pg_pengajian.visibility = View.VISIBLE
+
+        // inisialisasi database dan membuat nama folder path: di firebase
+        val db = FirebaseDatabase.getInstance().getReference("pengajian/$idPengajian")
+
+        // menambahkan pada constructor milik class Pengajia
+        db.setValue(Pengajian(idPengajian, pTema, pJudul, pTanggal, pJam, pIsi))
+            .addOnSuccessListener {
+                pg_pengajian.visibility = View.GONE
+                Toast.makeText(context, "Berhasil menambahkan data", Toast.LENGTH_SHORT).show()
+
+                et_pengajian_tema.text = null
+                et_pengajian_judul.text = null
+                tv_pengajian_tgl.text = "Tanggal Pengajian"
+                tv_pengajian_jam.text = "Waktu Pengajian"
+                et_pengajian_isi.text = null
+
+            }
+            .addOnFailureListener{
+                pg_pengajian.visibility = View.GONE
+                Toast.makeText(context, "Gagal menambahkan data", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun getJamPengajian() {
+        val calendar = Calendar.getInstance()
+
+        timePickerDialog = TimePickerDialog(
+            context as Context, OnTimeSetListener { _, hourOfDay, minute ->
+                tv_pengajian_jam.text = ("$hourOfDay:$minute")
+            },
+            // calender pertamakali di buka
+            calendar[Calendar.HOUR_OF_DAY], calendar[Calendar.MINUTE],
+            // cek format 24 jam
+            DateFormat.is24HourFormat(context as Context)
+        )
+        timePickerDialog?.show()
+    }
+
+    private fun getTanggalPengajian() {
+        val calendar = Calendar.getInstance()
+        val c_year = calendar.get(Calendar.YEAR)
+        val c_month = calendar.get(Calendar.MONTH)
+        val c_day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        datePickerDialog = DatePickerDialog(
+            context as Context, DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                tv_pengajian_tgl.text = ("$dayOfMonth-${month + 1}-$year")  //ditambah satu karena index MOUNT di mulai dari nol (0-11)
+            }, c_year, c_month, c_day
+        )
+        datePickerDialog?.show()
     }
 
 }
---
+++ app/src/main/java/com/zackyasgar/at_tauba/admin/Pengajian.kt
@@ -0,0 +1,12 @@
+package com.zackyasgar.at_tauba.admin
+
+class Pengajian(
+    var idPengajian: String,
+    var temaPengajian: String,
+    var judulPengajian: String,
+    var tanggalPengajian: String,
+    var jamPengajian: String,
+    var isiPengajian: String
+) {
+
+}
