e339aa83b249875ea6311a269c93d378587f7db8
arifahmadalfian <arifahmadalfian@gmail.com>
2020-08-30 21:16:37 +0700

    percobaan 5 mengambil data dari firestore fragment

--- app/src/main/java/com/zackyasgar/at_tauba/ui/home/HomeFragment.kt
+++ app/src/main/java/com/zackyasgar/at_tauba/ui/home/HomeFragment.kt
@@ -22,58 +22,14 @@ class HomeFragment : Fragment(){
     private var jumatList = jumatData.collection("jumatan")
     private lateinit var adapterJumat: JumatAdapter
 
-    //inisisal dari class Firebase Pengajian
-    private var firebasePengajian = PengajianFirestore()
-    private var listPengajian: List<Pengajian> = ArrayList()
-    private var pengajianListAdapter: PengajianAdapter = PengajianAdapter(listPengajian)
-    private lateinit var recyclerViewPengajian: RecyclerView
-    private var v: View? = null
-    private var v1: View? = null
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        this.v = inflater.inflate(R.layout.fragment_home, container, false)
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
         return v
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-         this.v1 = view
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        getPengajian()
-    }
-    private fun getPengajian() {
-        if(firebasePengajian.getUser() == null){
-            firebasePengajian.createUser().addOnCompleteListener {
-                if(it.isSuccessful){
-                    loadDataPengajian()
-                } else {
-                    Log.d("loadDataPengajian()", "Error : ${it.exception!!.message} ")
-                }
-            }
-        } else {
-            loadDataPengajian()
-            rv_pengajian.layoutManager = LinearLayoutManager(v?.context, LinearLayoutManager.HORIZONTAL, false)
-            rv_pengajian.setHasFixedSize(true)
-            rv_pengajian.adapter = pengajianListAdapter
-        }
-    }
-
-    private fun loadDataPengajian() {
-        firebasePengajian.getListPengajian().addOnCompleteListener {
-            if (it.isSuccessful) {
-                listPengajian = it.result!!.toObjects(Pengajian::class.java)
-                pengajianListAdapter.pengajianListItems = listPengajian
-                pengajianListAdapter.notifyDataSetChanged()
-            } else {
-                Log.d("loadDataPengajian()", "Error : ${it.exception!!.message} ")
-            }
-        }
-    }
 }
